7e43f6e4b847b3d8b8ad15caeb9e8f790bb3b200
Rolando Oc <rolandowc@hotmail.com>
2022-07-07 14:39:32 -0500

    Interface | Typed in the request

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.sass']
+  styleUrls: ['./results.component.sass'],
 })
 export class ResultsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  // getter
+  get results() {
+    return this.gifService.results;
   }
 
+  constructor(private gifService: GifsService) {}
+
+  ngOnInit(): void {}
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gifs, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -7,12 +8,14 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private apiKey: string = 'zOBibV9WHuqme4SXVSkd5Y0L5RJvKfP2';
   private _record: string[] = [];
+  // TODO change for type
+  public results: Gifs[] = [];
 
   // getter
   get record() {
     return [...this._record];
   }
-  constructor( private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   // methods
   addRecord(query: string): void {
@@ -26,8 +29,25 @@ export class GifsService {
       this._record = this._record.splice(0, 9);
     }
 
+    // this.searchRequest(query);
+    this.http
+      .get<SearchGifsResponse>(
+        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10&offset=0&rating=g&lang=es`
+      )
+      .subscribe((resp) => {
+        this.results = resp.data;
+      });
+  }
+
+  private searchRequest(query: string) {
     // request
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=zOBibV9WHuqme4SXVSkd5Y0L5RJvKfP2&q=Dragon+Ball+z&limit=10&offset=0&rating=g&lang=es')
-              .subscribe((resp:any)=> console.log(resp.data))
+    this.http
+      .get(
+        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10&offset=0&rating=g&lang=es`
+      )
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+        this.results = resp.data;
+      });
   }
 }
