36ca1b3600d365df8f89b9ce4ab9ed712a2cf865
Rolando Oc <rolandowc@hotmail.com>
2022-07-07 10:54:07 -0500

    Search | @ViewChild

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.sass']
+  styleUrls: ['./search.component.sass'],
 })
 export class SearchComponent implements OnInit {
+  // search selector with ViewChild
+  @ViewChild('textSearch') textSearch!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // methods
+  search() {
+    const inpuText = this.textSearch.nativeElement;
+    const searchInformation = inpuText.value;
+    console.log(searchInformation);
+    // clear input
+    inpuText.value = '';
   }
-
 }
